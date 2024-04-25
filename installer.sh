353ceb5b83e0e9002e4dbb7ceab6f83bc904b2fa
svderived <92538232+svderived@users.noreply.github.com>
2024-04-24 20:19:14 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,9 +5,9 @@ URL1="https://raw.githubusercontent.com/svderived/0g/main/0g-installer.sh"
 URL2="https://raw.githubusercontent.com/svderived/0g/main/0g-run.sh"
 URL3="https://raw.githubusercontent.com/svderived/0g/main/0g-wallet.sh"
 
-FILE1=get-peer-id.sh
-FILE2=pre.sh
-FILE3=post.sh
+FILE1=0g-installer.sh
+FILE2=0g-run.sh
+FILE3=0g-wallet.sh
 
 # Use curl to download the files
 curl -L $URL1 -o $FILE1
