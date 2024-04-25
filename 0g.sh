c4cfce28162be0174783d60004c5c3d2bbe689d1
svderived <92538232+svderived@users.noreply.github.com>
2024-04-24 20:42:55 -0700

    Update 0g.sh

--- 0g.sh
+++ 0g.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define the URLs of the files you want to download
-URL1="https://raw.githubusercontent.com/svderived/0g/main/0g-installer.sh"
-URL2="https://raw.githubusercontent.com/svderived/0g/main/0g-run.sh"
-URL3="https://raw.githubusercontent.com/svderived/0g/main/0g-wallet.sh"
+export URL1="https://raw.githubusercontent.com/svderived/0g/main/0g-installer.sh"
+export URL2="https://raw.githubusercontent.com/svderived/0g/main/0g-run.sh"
+export URL3="https://raw.githubusercontent.com/svderived/0g/main/0g-wallet.sh"
 
-FILE1=0g-installer.sh
-FILE2=0g-run.sh
-FILE3=0g-wallet.sh
+export FILE1=0g-installer.sh
+export FILE2=0g-run.sh
+export FILE3=0g-wallet.sh
 
 # Use curl to download the files
 curl -L $URL1 -o $FILE1
